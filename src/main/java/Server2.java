51e9013407359b795bf38c0063ccb2ea45d5d3d9
carcisa <mcisarr228@g.educaand.es>
2023-09-20 16:35:24 +0200

    Saludo personalizado con Servlet

---
+++ src/main/java/Server2.java
@@ -0,0 +1,57 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Server2
+ */
+public class Server2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Server2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        out.print("<h1>GET</h1>");
+        out.print("Hola, " + nombre + " " + apellido + "!");
+        
+        out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		procesaFormularioPOST(request, response);
+	}
+	
+	public void procesaFormularioPOST(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        out.print("<h1>POST</h1>");
+        out.print("Hola, " + nombre + " " + apellido + "!");
+        out.close();
+	}
+
+}
